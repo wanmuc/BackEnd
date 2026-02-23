3b9a9a5c0b461a01b7da8fffc7e4911fa4d66dff
KIDPhantom <xiangkaicheng@foxmail.com>
2026-02-23 22:22:53 +0800

    Fix: 修复了12章中判断字节序的代码

--- Chapter12/bigsmallcheck.cpp
+++ Chapter12/bigsmallcheck.cpp
@@ -8,12 +8,8 @@ bool IsSmall() {
     uint32_t data;
   };
   Check check;
-  for (int i = 0; i < 4; i++) {
-    check.bytes[i] = i;
-  }
-  std::cout << "data = " << check.data << std::endl;
-  uint8_t* data = (uint8_t*)&check.data;
-  return data[0] == 0 && data[1] == 1 && data[2] == 2 && data[3] == 3;
+  check.data = 1;
+  return check.bytes[0] == 1;
 }
 
 int main() {
